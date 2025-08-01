0a4e20afe2fd10c610208c1f21e0287367bba231
kwanjung <kwanjungkim@naver.com>
2025-08-01 14:24:07 +0900

    init : project init

---
+++ ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/ForkMeBeApplication.java
@@ -0,0 +1,13 @@
+package com.S1_K4.ForkMe_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkMeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkMeBeApplication.class, args);
+	}
+
+}
---
+++ ForkMe-BE/src/test/java/com/S1_K4/ForkMe_BE/ForkMeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S1_K4.ForkMe_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkMeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
