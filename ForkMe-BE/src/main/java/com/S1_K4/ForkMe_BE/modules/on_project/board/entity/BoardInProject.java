e54fe5819126bf8d67dbc40310f554d776bb454f
kws39 <kws3917@naver.com>
2025-08-04 12:09:19 +0900

    boardinproject entity

---
+++ ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/on_project/board/entity/BoardInProject.java
@@ -0,0 +1,8 @@
+package com.S1_K4.ForkMe_BE.modules.on_project.board.entity;/**
+* @author         : 김송이
+* @packageName    : com.S1_K4.ForkMe_BE.modules.on_project.board.entity
+* @fileName       : BoardInProject
+* @date           : 2025-08-04
+* @description    : 프로젝트 안 게시판 Entity 입니다. 
+*/public class BoardInProject {
+}
