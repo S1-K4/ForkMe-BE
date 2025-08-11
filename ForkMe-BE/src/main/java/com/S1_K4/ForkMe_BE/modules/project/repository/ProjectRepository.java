2ae8cb53decff0244009c88f647e44012f8173fd
SCOM <scomjoo@gmail.com>
2025-08-11 11:23:02 +0900

    feat: 유저기술스택 저장 기능 #33

--- ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/project/repository/ProjectRepository.java
+++ ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/project/repository/ProjectRepository.java
@@ -1,13 +1,10 @@
 package com.S1_K4.ForkMe_BE.modules.project.repository;
 
-import com.S1_K4.ForkMe_BE.modules.project.dto.CompletedProjectSummaryDto;
 import com.S1_K4.ForkMe_BE.modules.project.dto.SideBarProjectDto;
 import com.S1_K4.ForkMe_BE.modules.project.entity.Project;
-import com.S1_K4.ForkMe_BE.modules.project.entity.ProjectMember;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -27,11 +24,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
 
     @Query("""
-    SELECT p FROM Project p
-    JOIN FETCH p.user
-    LEFT JOIN FETCH p.projectProfile
-    WHERE p.projectPk = :projectPk
-    """)
+            SELECT p FROM Project p
+            JOIN FETCH p.user
+            LEFT JOIN FETCH p.projectProfile
+            WHERE p.projectPk = :projectPk
+            """)
     Optional<Project> findWithProfileAndUserByProjectPk(@Param("projectPk") Long projectPk);
 
     //프로젝트 목록 조회
@@ -55,15 +52,5 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
             "WHERE pm.user.userPk = :userPk AND p.projectStatus IN ('IN_PROGRESS','ADDING') AND p.deletedYN = 'N'")
     List<SideBarProjectDto> findProgressProjectsByUser(@Param("userPk") Long userPk);
 
-
-    @Query("SELECT new com.S1_K4.ForkMe_BE.modules.project.dto.CompletedProjectSummaryDto(" +
-            "p.projectPk, p.projectTitle, " +
-            "p.projectStartDate, p.projectEndDate, p.projectStatus," +
-            "pp.projectProfilePk, pp.projectProfileTitle, pp.progressType) " +
-            "FROM ProjectMember pm, ProjectProfile pp " + // 1. 필요한 엔티티들을 FROM 절에 나열
-            "JOIN pm.project p " +
-            "WHERE pp.project = p " + // 2. WHERE 절에서 ProjectProfile과 Project를 연결
-            "AND pm.user.userPk = :userPk AND p.projectStatus = 'COMPLETED' AND p.deletedYN = 'N'")
-    List<CompletedProjectSummaryDto> findCompletedProjectsByUserPk(@Param("userPk") Long userPk);
 
 }
