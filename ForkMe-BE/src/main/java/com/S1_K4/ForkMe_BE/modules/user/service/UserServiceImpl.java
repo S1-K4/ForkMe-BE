f3feb53c9f6fbea1ee37ef20c2eab4836babe0cb
SCOM <scomjoo@gmail.com>
2025-08-11 11:17:20 +0900

    feat: 유저기술스택 저장 기능 #33

--- ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/user/service/UserServiceImpl.java
+++ ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/user/service/UserServiceImpl.java
@@ -7,9 +7,10 @@ import com.S1_K4.ForkMe_BE.modules.user.entity.User;
 import com.S1_K4.ForkMe_BE.modules.user.entity.UserTechStack;
 import com.S1_K4.ForkMe_BE.modules.user.repository.UserTechStackRepository;
 import com.S1_K4.ForkMe_BE.reference.stack.entity.TechStack;
-import com.S1_K4.ForkMe_BE.reference.stack.repository.TechStackRepository;
+import com.S1_K4.ForkMe_BE.reference.stack.repository.StackRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,13 +22,13 @@ import java.util.List;
  * @date : 2025-08-06
  * @description : 유저 서비스
  */
-
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserTechStackRepository userTechStackRepository;
-    private final TechStackRepository techStackRepository;
+    private final StackRepository stackRepository;
     private final ProjectRepository projectRepository;
 
     @Override
@@ -42,8 +43,6 @@ public class UserServiceImpl implements UserService {
         List<SideBarProjectDto> workSpace = projectRepository.findProgressProjectsByUser(userPk);
 
 
-
-
         return SidebarResponseDto.builder()
                 .preparingProjectList(preparingProject)
                 .workSpaceList(workSpace)
@@ -54,12 +53,14 @@ public class UserServiceImpl implements UserService {
     //@Override
     @Transactional
     public void updateUserTechStack(User user, List<Long> techStackList) {
+        log.info("updateUserTechStack : " + user.getUserPk() + " / " + techStackList);
         // 기존 기술 스택 삭제
         userTechStackRepository.deleteAllByUser(user);
+        log.info("기존 기술 스택 삭제");
 
         // 새로운 기술 스택 저장
         if (techStackList != null && !techStackList.isEmpty()) {
-            List<TechStack> techStacks = techStackRepository.findAllById(techStackList);
+            List<TechStack> techStacks = stackRepository.findAllById(techStackList);
             List<UserTechStack> userTechStacks = techStacks.stream()
                     .map(stack -> new UserTechStack(user, stack))
                     .toList();
