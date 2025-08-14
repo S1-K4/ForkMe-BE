1f2491701d58d5b77f590d9ffbf889b989b98208
kwanjung <kwanjungkim@naver.com>
2025-08-14 17:40:43 +0900

    fix : success handler 리다이렉트 url 수정

--- ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/auth/OAuth2AuthenticationSuccessHandler.java
+++ ForkMe-BE/src/main/java/com/S1_K4/ForkMe_BE/modules/auth/OAuth2AuthenticationSuccessHandler.java
@@ -140,8 +140,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         refreshTokenCookie.setPath("/");
         response.addCookie(refreshTokenCookie);
 
-        //String url = "http://forkme.site:3000/login-success.html";
-        String url = "http://localhost:3000/login-success.html";
+        String url = "http://forkme.site:3000/login-success.html";
+        //String url = "http://localhost:3000/login-success.html";
         String targetUrl = UriComponentsBuilder.fromUriString(url)
                 .queryParam("accessToken", accessToken)
                 .build().toUriString();
